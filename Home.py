926898df4312793a343d8b3521779743de33b101
Quera <70485959+Quera-fr@users.noreply.github.com>
2024-12-18 09:24:28 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,8 +1,28 @@
 import streamlit as st
 
-
 # Création d'un titre
 st.title('Interface-Mistral-AI')
 
 # Création d'un sous-titre
 st.subheader("Mistral AI")
+
+# Création d'une zone de texte
+st.write("Introduction à mistral AI")
+
+st.write("""
+
+# Titre
+## Sous-titre
+
+**Text**
+
+  `print("Hello World")`
+
+""")
+
+# Création d'un bouton
+
+
+# Cration d'un slider 
+
+# Lecture d'un fichier csv avec pandas
