b954da27a2b11c997542104e82ddd91c5199adbf
Quera <70485959+Quera-fr@users.noreply.github.com>
2024-12-18 09:41:10 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -31,10 +31,10 @@ if st.button("Press OK"):
 
 
 # Image
-st.sidebare.image('https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png')
+st.sidebar.image('https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png')
 
 # Video
-st.sidebare.video("https://www.youtube.com/watch?v=sgnrL7yo1TE")
+st.sidebar.video("https://www.youtube.com/watch?v=sgnrL7yo1TE")
 
 # Cration d'un slider 
 
